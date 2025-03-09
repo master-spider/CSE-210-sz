fdd8951c7eca34c20b7574af360cf8362956555b
master-spider <tshlase@gmail.com>
2025-03-09 16:42:30 +0000

    Completed W01 Exercise 1 printing and taking user input

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Ask for user first name
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+        
+        // Ask for user last name
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Print out the user's full name in James Bond' Style
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
