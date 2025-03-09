8aacc6977aeb3aaee5a0fdb0ca47ad6a7e871a97
master-spider <tshlase@gmail.com>
2025-03-09 17:34:45 +0000

    Just completed W01 Exercise 2 if statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,42 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+    {   
+        // Ask for user's grade
+        Console.Write("Please enter your percentage grade: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congradulations, you have passed!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you have failed, try again.");
+        }
     }
 }
