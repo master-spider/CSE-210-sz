dba4f9160dcaf18cdb6441bdb039c9a82b8f77c7
master-spider <tshlase@gmail.com>
2025-03-09 21:41:01 +0000

    Just completed W01 Exercise 4 Lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,36 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+    List <int> numbers = new List<int>();
+    int total = 0;
+    int number = -1;
+    Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+    while (number != 0)
+    {
+      Console.Write("Enter a number: ");
+      number = int.Parse(Console.ReadLine());
+      if (number != 0)
+      {
+          numbers.Add(number);
+          total += number;
+      }
+    }
+    float avarage = ((float)total) / numbers.Count;
+    // find the maximum number
+    int max = numbers[0];
+    for (int i = 1; i < numbers.Count; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    }
+    Console.WriteLine($"The sum is: {total}");
+    Console.WriteLine($"The avarage is: {avarage}");
+    Console.WriteLine($"The largest number is: {max}");
+
     }
 }
