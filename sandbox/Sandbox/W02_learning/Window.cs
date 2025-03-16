18557930338b34f8cefeed78a150d88ad94517c6
master-spider <tshlase@gmail.com>
2025-03-16 21:34:50 +0000

    Renaming windowsclass to window

---
+++ sandbox/Sandbox/W02_learning/Window.cs
@@ -0,0 +1,12 @@
+public class Window //A new data type like string or int
+{
+    public double _width;
+    public double _height;
+    public string _color;
+    
+    public double GetArea()
+    {
+        return _width * _height;
+    }
+
+}
