adc145b937c754d531c079b96cce1cce0a35958e
master-spider <tshlase@gmail.com>
2025-03-16 23:19:29 +0000

    added house object to use window objects in it.

---
+++ sandbox/Sandbox/W02_learning/House.cs
@@ -0,0 +1,10 @@
+// Objects within objects - creating a house
+// object that will contain the room objects
+
+public class House
+{
+    public string _owner;
+    public Window _bathroom;
+    public Window _bedroom;
+
+}
